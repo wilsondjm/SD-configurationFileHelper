7eb9e35063484b0b9b4e534d9433563baf70411a
wilsondjm <wsen.huang@gmail.com>
2014-07-08 09:47:16 +0800

    Leverage log4net to write trace logs in logfile

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -12,6 +12,8 @@ namespace WebApplication1
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            log4net.Config.XmlConfigurator.Configure();
+            
         }
     }
 }
